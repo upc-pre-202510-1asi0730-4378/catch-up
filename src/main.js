c893e1b86e1ed3e34f275d47095526675aef11a1
Juan Antonio Flores <pcsijflo@upc.edu.pe>
2025-04-24 16:49:02 -0700

    feat(material-design): add material design support with primevue.

--- src/main.js
+++ src/main.js
@@ -2,8 +2,23 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import i18n from "./i18n.js";
+import PrimeVue from 'primevue/config';
+import Material from '@primeuix/themes/material';
+import 'primeicons/primeicons.css';
+import 'primeflex/primeflex.css';
+import {Avatar, Button, Card, Drawer, Menu, Toolbar, Tooltip} from "primevue";
 
 const app = createApp(App);
 
 app.use(i18n)
+    .use(PrimeVue, { ripple: true, theme: { preset: Material } })
+    .component('pv-button', Button)
+    .component('pv-card', Card)
+    .component('pv-select-button', Button)
+    .component('pv-drawer', Drawer)
+    .component('pv-avatar', Avatar)
+    .component('pv-menu', Menu)
+    .component('pv-toolbar', Toolbar)
+    .component('pv-tooltip', Tooltip)
+    .component('pv-image', Image)
     .mount('#app');
