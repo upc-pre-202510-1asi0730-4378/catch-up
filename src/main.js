0fd967eedb0d1eb78133a2e1934c782ee4f61f75
Juan Antonio Flores <pcsijflo@upc.edu.pe>
2025-04-24 16:31:38 -0700

    feat(i18n): add internationalization support.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import i18n from "./i18n.js";
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app.use(i18n)
+    .mount('#app');
