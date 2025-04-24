ebd87265b35f4c8e783b04cf91f2273a37842ee2
Juan Antonio Flores <pcsijflo@upc.edu.pe>
2025-04-24 16:53:14 -0700

    feat(i18n): add language switcher component.

---
+++ src/public/components/language-switcher.component.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  name: "language-switcher",
+  data() {
+    return {
+      languages: ['en', 'es'],
+    }
+  }
+}
+</script>
+
+<template>
+  <pv-select-button v-model="$i18n.locale" :options="languages" class="uppercase"/>
+</template>
+
+<style scoped>
+
+</style>
