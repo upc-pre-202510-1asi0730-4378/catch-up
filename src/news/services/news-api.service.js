62c60eda096cd92351b334a89834121a422c865f
Angel Velasquez <pcisavel@upc.edu.pe>
2025-04-25 17:40:26 -0700

    feat(news): add news api service client.

---
+++ src/news/services/news-api.service.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+const newsApiUrl = import.meta.env.VITE_NEWS_API_URL;
+const apiKey = import.meta.env.VITE_NEWS_API_KEY;
+const sourcesEndpoint = import.meta.env.VITE_SOURCES_ENDPOINT;
+const topHeadlinesEndpoint = import.meta.env.VITE_TOP_HEADLINES_ENDPOINT;
+
+const http = axios.create({
+    baseURL: newsApiUrl,
+    params: { apiKey: apiKey },
+});
+
+export class NewsApiService {
+    getSources() {
+        return http.get(`${sourcesEndpoint}`);
+    }
+    
+    getArticlesForSourceId(sourceId) {
+        return http.get(`${topHeadlinesEndpoint}`, {params: {sources: sourceId}});
+    }
+}
